8be027f78114f7e205aa46428526a2af7ba9979c
MiNa SaLib <99540302+MiNa-SaLib@users.noreply.github.com>
2022-07-11 00:07:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -96,18 +96,67 @@ function age() {
     }
   }
 
-  if (day != "" && month != "" && year != "") {
-    document.getElementById("age").innerHTML = `Your Age is ${yearsinif} Year 
-     , ${monthinif} Month And  ${daysinif} Day`;
-    if (day == get_day && month == get_month) {
+  if (
+    day != "" &&
+    month != "" &&
+    year != "" &&
+    day != 0 &&
+    month != 0 &&
+    year != 0 &&
+    day != " " &&
+    month != " " &&
+    year != " " &&
+    day <= 31 &&
+    month <= 12 &&
+    day >= 1 &&
+    month >= 1 &&
+    get_year >= year
+  ) {
+    document.getElementById(
+      "age"
+    ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+     , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Day
+     <h3>
+      Remain  <span id="id22">${11 - monthinif}</span>
+      Month And <span id="id33">${
+        31 - daysinif
+      } </span> Day to your next birthday
+      </h3>
+     `;
+
+    document.getElementById("id1").style.color = "red";
+    document.getElementById("id2").style.color = "red";
+    document.getElementById("id3").style.color = "red";
+    document.getElementById("id22").style.color = "red";
+    document.getElementById("id33").style.color = "red";
+    document.getElementById("id1").style.fontSize = "25px";
+    document.getElementById("id2").style.fontSize = "25px";
+    document.getElementById("id3").style.fontSize = "25px";
+
+    if (day == get_day && month == get_month && get_year >= year) {
       document.getElementById(
         "age1"
-      ).innerHTML = `Today is Your  Birthday, Happy Birthday`;
+      ).innerHTML = `Today is Your  Birthday, Happy Birthday <div><audio
+      src="Happy-Birthday-To-You-Happy-Birthday-Songs-2021-WishesPlus.com_.mp3"
+      controls
+      autoplay
+      style="display: none"
+     ></audio></div> <img src="happy_birthday_PNG1.png" style="width: 250px;margin-top:50px;" alt="" /> `;
+    }
+
+    if (day == get_day + 1 && month == get_month && get_year >= year) {
+      document.getElementById(
+        "age1"
+      ).innerHTML = `Tomorrow is Your  Birthday, Happy Birthday <div><audio
+      src="Happy-Birthday-To-You-Happy-Birthday-Songs-2021-WishesPlus.com_.mp3"
+      controls
+      autoplay
+      style="display: none"
+    ></audio></div><img src="happy_birthday_PNG1.png" style="width: 250px;margin-top:50px;" alt="" />  `;
     }
 
     document.getElementById("name").innerHTML = `By : MiNa SaLib`;
-  }
-  if (day == "" && month == "" && year == "") {
+  } else {
     alert(`Please Enter Avalid Age !`);
   }
 }
