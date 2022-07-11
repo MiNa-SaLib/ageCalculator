6484da8e2092015fd61dbd6401e009284fac88dd
MiNa SaLib <99540302+MiNa-SaLib@users.noreply.github.com>
2022-07-11 17:41:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function age() {
   let monthinif = 0;
   let yearsinif = 0;
 
-  if (year < get_year) {
+  if (year <= get_year) {
     if (month == get_month) {
       if (day == get_day) {
         monthinif = get_month - month;
@@ -96,7 +96,36 @@ function age() {
     }
   }
 
-  if (
+  let daystring = day.toString();
+  let monthstring = month.toString();
+  let yearstring = year.toString();
+  let ramaindays = 30 - daysinif;
+  let ramainmonths = 11 - monthinif;
+  if (day > 29 && month == 2)
+    alert(
+      `February doesn't enclude ${day} days , Please Enter Avalid Date !`
+    ); //2
+  else if (day > 30 && month == 4)
+    alert(`April doesn't enclude ${day} days , Please Enter Avalid Age !`); //4
+  else if (day > 30 && month == 6)
+    alert(`june doesn't enclude ${day} days , Please Enter Avalid Age !`); //6
+  else if (day > 30 && month == 9)
+    alert(
+      `September doesn't enclude ${day} days , Please Enter Avalid Age !`
+    ); //9
+  else if (day > 30 && month == 11)
+    alert(
+      `November doesn't enclude ${day} days , Please Enter Avalid Age !`
+    ); //11
+  else if (year > get_year)
+    alert(`Your Date of birth is invalid , Please Enter Avalid Date !`);
+  else if (
+    daystring[0] != "+" &&
+    monthstring[0] != "+" &&
+    yearstring[0] != "+" &&
+    daystring[0] != "-" &&
+    monthstring[0] != "-" &&
+    yearstring[0] != "-" &&
     day != "" &&
     month != "" &&
     year != "" &&
@@ -109,20 +138,385 @@ function age() {
     day <= 31 &&
     month <= 12 &&
     day >= 1 &&
-    month >= 1 &&
-    get_year >= year
+    month >= 1
   ) {
-    document.getElementById(
-      "age"
-    ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
-     , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Day
-     <h3>
-      Remain  <span id="id22">${11 - monthinif}</span>
-      Month And <span id="id33">${
-        31 - daysinif
-      } </span> Day to your next birthday
-      </h3>
-     `;
+    //if the year is 1
+
+    if (yearsinif <= 1) {
+      //1
+      if (daysinif <= 1 && monthinif <= 1) {
+        if (ramainmonths <= 1 && ramaindays <= 1) {
+          //1.1
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+          , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Day
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Month And <span id="id33">${ramaindays} </span> Day to your next birthday
+            </h3>`;
+        } else if (ramainmonths > 1 && ramaindays <= 1) {
+          //1.2
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+          , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Day
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Months And <span id="id33">${ramaindays} </span> Day to your next birthday
+            </h3>`;
+        } else if (ramainmonths <= 1 && ramaindays > 1) {
+          //1.3
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+          , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Day
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Month And <span id="id33">${ramaindays} </span> Days to your next birthday
+            </h3>`;
+        } else if (ramainmonths > 1 && ramaindays > 1) {
+          //1.4
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+          , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Day
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Months And <span id="id33">${ramaindays} </span> Days to your next birthday
+            </h3>
+          `;
+        }
+      }
+      //2
+      else if (daysinif > 1 && monthinif > 1) {
+        if (ramainmonths <= 1 && ramaindays <= 1) {
+          //2.1
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+          , <span id='id2'>${monthinif}</span> Months And  <span id='id3'>${daysinif}</span> Days
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Month And <span id="id33">${ramaindays} </span> Day to your next birthday
+            </h3>
+          `;
+        } else if (ramainmonths > 1 && ramaindays <= 1) {
+          //2.2
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+          , <span id='id2'>${monthinif}</span> Months And  <span id='id3'>${daysinif}</span> Days
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Months And <span id="id33">${ramaindays} </span> Day to your next birthday
+            </h3>
+          `;
+        } else if (ramainmonths <= 1 && ramaindays > 1) {
+          //2.3
+          2.3;
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+          , <span id='id2'>${monthinif}</span> Months And  <span id='id3'>${daysinif}</span> Days
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Month And <span id="id33">${ramaindays} </span> Days to your next birthday
+            </h3>
+          `;
+        } else if (ramainmonths > 1 && ramaindays > 1) {
+          //2.4
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+          , <span id='id2'>${monthinif}</span> Months And  <span id='id3'>${daysinif}</span> Days
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Months And <span id="id33">${ramaindays} </span> Days to your next birthday
+            </h3>
+          `;
+        }
+      }
+
+      //3
+      else if (daysinif > 1 && monthinif <= 1) {
+        if (ramainmonths <= 1 && ramaindays <= 1) {
+          //3.1
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+          , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Days
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Month And <span id="id33">${ramaindays} </span> Day to your next birthday
+            </h3>`;
+        } else if (ramainmonths > 1 && ramaindays <= 1) {
+          //3.2
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+          , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Days
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Months And <span id="id33">${ramaindays} </span> Day to your next birthday
+            </h3>`;
+        } else if (ramainmonths <= 1 && ramaindays > 1) {
+          //3.3
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+          , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Days
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Month And <span id="id33">${ramaindays} </span> Days to your next birthday
+            </h3>`;
+        } else {
+          //3.4
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+          , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Days
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Months And <span id="id33">${ramaindays} </span> Days to your next birthday
+            </h3>
+          `;
+        }
+      }
+      //4
+      else if (daysinif <= 1 && monthinif > 1) {
+        if (ramainmonths <= 1 && ramaindays <= 1) {
+          //4.1
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+          , <span id='id2'>${monthinif}</span> Months And  <span id='id3'>${daysinif}</span> Day
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Month And <span id="id33">${ramaindays} </span> Day to your next birthday
+            </h3>`;
+        } else if (ramainmonths > 1 && ramaindays <= 1) {
+          //4.2
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+          , <span id='id2'>${monthinif}</span> Months And  <span id='id3'>${daysinif}</span> Day
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Months And <span id="id33">${ramaindays} </span> Day to your next birthday
+            </h3>`;
+        } else if (ramainmonths <= 1 && ramaindays > 1) {
+          //4.3
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+          , <span id='id2'>${monthinif}</span> Months And  <span id='id3'>${daysinif}</span> Day
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Month And <span id="id33">${ramaindays} </span> Days to your next birthday
+            </h3>`;
+        } else {
+          //4.4
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Year 
+          , <span id='id2'>${monthinif}</span> Months And  <span id='id3'>${daysinif}</span> Day
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Months And <span id="id33">${ramaindays} </span> Days to your next birthday
+            </h3>`;
+        }
+      }
+    }
+
+    //if the year is greater than 1
+
+    if (yearsinif > 1) {
+      //1
+      if (daysinif <= 1 && monthinif <= 1) {
+        if (ramainmonths <= 1 && ramaindays <= 1) {
+          //1.1
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Years 
+          , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Day
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Month And <span id="id33">${ramaindays} </span> Day to your next birthday
+            </h3>`;
+        } else if (ramainmonths > 1 && ramaindays <= 1) {
+          //1.2
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Years 
+          , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Day
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Months And <span id="id33">${ramaindays} </span> Day to your next birthday
+            </h3>`;
+        } else if (ramainmonths <= 1 && ramaindays > 1) {
+          //1.3
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Years 
+          , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Day
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Month And <span id="id33">${ramaindays} </span> Days to your next birthday
+            </h3>`;
+        } else {
+          //2.4
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Years 
+          , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Day
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Months And <span id="id33">${ramaindays} </span> Days to your next birthday
+            </h3>
+          `;
+        }
+      }
+      //2
+      else if (daysinif > 1 && monthinif > 1) {
+        if (ramainmonths <= 1 && ramaindays <= 1) {
+          //2.1
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Years 
+          , <span id='id2'>${monthinif}</span> Months And  <span id='id3'>${daysinif}</span> Days
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Month And <span id="id33">${ramaindays} </span> Day to your next birthday
+            </h3>
+          `;
+        } else if (ramainmonths > 1 && ramaindays <= 1) {
+          //2.2
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Years 
+          , <span id='id2'>${monthinif}</span> Months And  <span id='id3'>${daysinif}</span> Days
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Months And <span id="id33">${ramaindays} </span> Day to your next birthday
+            </h3>
+          `;
+        } else if (ramainmonths <= 1 && ramaindays > 1) {
+          //2.3
+          2.3;
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Years 
+          , <span id='id2'>${monthinif}</span> Months And  <span id='id3'>${daysinif}</span> Days
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Month And <span id="id33">${ramaindays} </span> Days to your next birthday
+            </h3>
+          `;
+        } else {
+          //2.4
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Years 
+          , <span id='id2'>${monthinif}</span> Months And  <span id='id3'>${daysinif}</span> Days
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Months And <span id="id33">${ramaindays} </span> Days to your next birthday
+            </h3>
+          `;
+        }
+      }
+
+      //3
+      else if (daysinif > 1 && monthinif <= 1) {
+        if (ramainmonths <= 1 && ramaindays <= 1) {
+          //3.1
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Years 
+          , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Days
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Month And <span id="id33">${ramaindays} </span> Day to your next birthday
+            </h3>`;
+        } else if (ramainmonths > 1 && ramaindays <= 1) {
+          //3.2
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Years 
+          , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Days
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Months And <span id="id33">${ramaindays} </span> Day to your next birthday
+            </h3>`;
+        } else if (ramainmonths <= 1 && ramaindays > 1) {
+          //3.3
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Years 
+          , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Days
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Month And <span id="id33">${ramaindays} </span> Days to your next birthday
+            </h3>`;
+        } else {
+          //3.4
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Years 
+          , <span id='id2'>${monthinif}</span> Month And  <span id='id3'>${daysinif}</span> Days
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Months And <span id="id33">${ramaindays} </span> Days to your next birthday
+            </h3>
+          `;
+        }
+      }
+      //4
+      else if (daysinif <= 1 && monthinif > 1) {
+        if (ramainmonths <= 1 && ramaindays <= 1) {
+          //4.1
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Years 
+          , <span id='id2'>${monthinif}</span> Months And  <span id='id3'>${daysinif}</span> Day
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Month And <span id="id33">${ramaindays} </span> Day to your next birthday
+            </h3>`;
+        } else if (ramainmonths > 1 && ramaindays <= 1) {
+          //4.2
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Years 
+          , <span id='id2'>${monthinif}</span> Months And  <span id='id3'>${daysinif}</span> Day
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Months And <span id="id33">${ramaindays} </span> Day to your next birthday
+            </h3>`;
+        } else if (ramainmonths <= 1 && ramaindays > 1) {
+          //4.3
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Years 
+          , <span id='id2'>${monthinif}</span> Months And  <span id='id3'>${daysinif}</span> Day
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Month And <span id="id33">${ramaindays} </span> Days to your next birthday
+            </h3>`;
+        } else {
+          //4.4
+          document.getElementById(
+            "age"
+          ).innerHTML = `Your Age is <span id='id1'>${yearsinif}</span> Years 
+          , <span id='id2'>${monthinif}</span> Months And  <span id='id3'>${daysinif}</span> Day
+          <h3>
+            Remain  <span id="id22">${ramainmonths}</span>
+            Months And <span id="id33">${ramaindays} </span> Days to your next birthday
+            </h3>`;
+        }
+      }
+    }
 
     document.getElementById("id1").style.color = "red";
     document.getElementById("id2").style.color = "red";
@@ -159,4 +553,5 @@ function age() {
   } else {
     alert(`Please Enter Avalid Age !`);
   }
+
 }
